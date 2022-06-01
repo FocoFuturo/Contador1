5a5a1f0382b9052151980ff698e04775eba26316
FocoFuturo <jj.focofuturo@gmail.com>
2022-05-31 23:17:59 -0300

    confirmar em master

--- script.js
+++ script.js
@@ -7,91 +7,94 @@ var minuto = 0;
 var segundo = 0;
 var hora = 0;
 
-function IniciarCronometro(valor){
+function IniciarCronometro(valor) {
 	this.evento = valor;
-    this.configHora = document.getElementById('hor').value;
+	this.configHora = document.getElementById('hor').value;
 	this.configMinuto = document.getElementById('min').value;
 	this.configSegundo = document.getElementById('seg').value;
 	this.mostrarValor = document.getElementById('mostrarValor');
-	
-	
-	if (evento=="start"){
-		if(!document.getElementById('hor').readOnly){
-            if(!this.validarNumero(this.configHora)){
-				alert("Campo hora não é um número!");
-            		return;
-                }
 
-			if(!this.validarNumero(this.configMinuto) || document.getElementById('min').value > 59){
-				alert("Campo minuto não é um número!");
+
+	if (evento == "start") {
+		if (!document.getElementById('hor').readOnly) {
+			if (!this.validarNumero(this.configHora) || document.getElementById('hor').value > 24) {
+				alert("Campo hora não é um número!");
 				return;
 			}
-			if(!this.validarNumero(this.configSegundo) || document.getElementById('seg').value > 59){
+
+			if (!this.validarNumero(this.configMinuto) || document.getElementById('min').value > 59) {
+				alert("Campo minuto não é um número (0 a 59)!");
+				return;
+			}
+			if (!this.validarNumero(this.configSegundo) || document.getElementById('seg').value > 59) {
 				alert("Campo segundo não é um número válido (0 a 59)!");
 				return;
 			}
 			document.getElementById('hor').readOnly = true;
 			document.getElementById('min').readOnly = true;
 			document.getElementById('seg').readOnly = true;
-			document.getElementById('btnIniciar').disabled  = true;
-			document.getElementById('btnResetar').disabled  = false;
-			document.getElementById('btnPausar').disabled  = false;
-            this.hora = document.getElementById('hor').value;
+			document.getElementById('btnIniciar').disabled = true;
+			document.getElementById('btnResetar').disabled = false;
+			document.getElementById('btnPausar').disabled = false;
+			this.hora = document.getElementById('hor').value;
 			this.minuto = document.getElementById('min').value;
 			this.segundo = document.getElementById('seg').value;
-			
+
 			document.getElementById('mostrarValor').classList.remove('mostrarValor');
 			document.getElementById('mostrarValor').classList.add('mostrarValor2');
 			document.getElementById('exibe').classList.remove('Classexibe');
 			document.getElementById('exibe').classList.add('Classexibe2');
-			
-		}else{
-            if(this.minuto== 0 && this.hora !=0 ){
-                this.minuto = 59;
-                this.hora--;
 
-            }
-			else if(this.segundo == 0 && this.minuto != 0){
+		} else {
+			if (  this.hora != 0 && this.minuto == 0 &&  this.segundo == 0) {
+				this.minuto = 60;
+				this.hora--;
+
+			}
+			if ( this.minuto != 0 && this.segundo == 0 ) {
+				
 				this.segundo = 59;
 				this.minuto--;
-			}else{
+				
+			} else {
+				
 				this.segundo--;
 			}
 
-			if(this.minuto == 0 && this.segundo == 0){
-                document.getElementById('hor').readOnly = false;
+			if ( this.hora == 0 && this.minuto == 0 && this.segundo == 0) {
+				document.getElementById('hor').readOnly = false;
 				document.getElementById('min').readOnly = false;
 				document.getElementById('seg').readOnly = false;
-				document.getElementById('btnIniciar').disabled  = false;
-				document.getElementById('btnResetar').disabled  = true;
-				document.getElementById('btnPausar').disabled  = true;
+				document.getElementById('btnIniciar').disabled = false;
+				document.getElementById('btnResetar').disabled = true;
+				document.getElementById('btnPausar').disabled = true;
 				this.mostrarValor.value = "00:00:00";
-				
+
 				document.getElementById('mostrarValor').classList.remove('mostrarValor2');
 				document.getElementById('mostrarValor').classList.add('mostrarValor');
 				document.getElementById('exibe').classList.remove('Classexibe2');
 				document.getElementById('exibe').classList.add('Classexibe');
-			
+
 				clearTimeout(this.contador);
 				return;
-			}	
-			
-            novoHora = null;
+			}
+
+			novoHora = null;
 			novoMinuto = null;
 			novoSegundo = null;
-            if(this.hora <= 9){
+			if (this.hora <= 9) {
 				novoHora = "0" + this.hora;
-			}else{
+			} else {
 				novoHora = this.hora;
 			}
-			if(this.minuto <= 9){
+			if (this.minuto <= 9) {
 				novoMinuto = "0" + this.minuto;
-			}else{
+			} else {
 				novoMinuto = this.minuto;
 			}
-			if(this.segundo <= 9){
+			if (this.segundo <= 9) {
 				novoSegundo = "0" + this.segundo;
-			}else{
+			} else {
 				novoSegundo = this.segundo;
 			}
 			this.mostrarValor.value = novoHora + ":" + novoMinuto + ":" + novoSegundo;
@@ -101,33 +104,33 @@ function IniciarCronometro(valor){
 	this.contador = setTimeout('IniciarCronometro(evento)', 1000);
 }
 
-function validarNumero(valor){
+function validarNumero(valor) {
 	return !isNaN(parseFloat(valor)) && isFinite(valor);
 }
 
 
 
-function PausarCronometro(){
-	if(document.getElementById('btnPausar').value=="PAUSAR"){
+function PausarCronometro() {
+	if (document.getElementById('btnPausar').value == "PAUSAR") {
 		document.getElementById('btnPausar').value = "VOLTAR";
 		this.evento = "pause";
-	}else{
+	} else {
 		document.getElementById('btnPausar').value = "PAUSAR";
 		this.evento = "start";
 	}
 }
 
-function ResetarCronometro(){
+function ResetarCronometro() {
 	document.getElementById('hor').readOnly = false;
 	document.getElementById('min').readOnly = false;
 	document.getElementById('seg').readOnly = false;
-	document.getElementById('btnIniciar').disabled  = false;
-	document.getElementById('btnResetar').disabled  = true;
-	document.getElementById('btnPausar').disabled  = true;
+	document.getElementById('btnIniciar').disabled = false;
+	document.getElementById('btnResetar').disabled = true;
+	document.getElementById('btnPausar').disabled = true;
 	document.getElementById('btnPausar').value = "PAUSAR";
 	this.mostrarValor.value = "00:00:00";
 
-document.getElementById('mostrarValor').classList.remove('mostrarValor2');
+	document.getElementById('mostrarValor').classList.remove('mostrarValor2');
 	document.getElementById('mostrarValor').classList.add('mostrarValor');
 	document.getElementById('exibe').classList.remove('Classexibe2');
 	document.getElementById('exibe').classList.add('Classexibe');
